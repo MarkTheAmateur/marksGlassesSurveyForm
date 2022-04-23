c33d5b47ec2da551bb9f408ecebb6905e78dda23
MarkTheAmateur <103880624+MarkTheAmateur@users.noreply.github.com>
2022-04-23 14:52:15 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# freeCodeCamp's Survey Form Project Starter Code
+Starter code for freeCodeCamp's "Survey Form" project made by me.
---
+++ app.js
@@ -0,0 +1,8 @@
+// Code by Amateur Developer (Mark Nguyen).
+// Initially created for freeCodeCamp's project.
+
+// CREDITS:
+// - CDN Code Testing Kit by freeCodeCamp
+// - Fonts on fonts.google.com
+
+const projectName = 'survey-form';
